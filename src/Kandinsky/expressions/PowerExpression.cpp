1d14c33eaf6e43a477e325cc2b8ab54e9aaf9252
Luca Pasquale <luca.pasquale@moxoff.com>
2017-03-30 15:41:32 +0200

    Fixed PowerExpression derivative when base is zero

--- src/Kandinsky/expressions/PowerExpression.cpp
+++ src/Kandinsky/expressions/PowerExpression.cpp
@@ -3,15 +3,20 @@
 #include <Kandinsky/expressions/MultiplicationExpression.hpp>
 #include <Kandinsky/expressions/DivisionExpression.hpp>
 #include <Kandinsky/expressions/AdditionExpression.hpp>
+#include <Kandinsky/expressions/SubtractionExpression.hpp>
+#include <Kandinsky/expressions/IfThenElseExpression.hpp>
+#include <Kandinsky/expressions/comparison/NotEqualExpression.hpp>
 #include <Kandinsky/expressions/BaseExpression.tpp>
 
 namespace Kandinsky
 {
     BaseExpressionPtr PowerExpression::derivative(const VariableExpressionPtr& variable) const
     {
-        return makePtr(pow(m_arg1, m_arg2) * (
+        return makePtr(ifThenElse(m_arg2->derivative(variable) != 0,
+                pow(m_arg1, m_arg2) * (
                 m_arg2->derivative(variable) * log(m_arg1) +
-                m_arg2 * m_arg1->derivative(variable) / m_arg1));
+                m_arg2 * m_arg1->derivative(variable) / m_arg1),
+                m_arg2 * m_arg1->derivative(variable) * pow(m_arg1, m_arg2 - 1)));
     }
 }
 
--- src/test/expressions/differentiation/PowerDifferentiationTest.cpp
+++ src/test/expressions/differentiation/PowerDifferentiationTest.cpp
@@ -10,6 +10,8 @@ int main (int /*argc*/, char** /*argv*/)
     x.setValue(3);
     assertOrExit(std::abs((pow(x, 5)).differentiate(x).evaluate() - 405) < 1.e-4, "x = 3; d(pow(x,5))/dx != 405");
     assertOrExit(std::abs((pow(4, x)).differentiate(x).evaluate() - 64*std::log(4)) < 1.e-4, "x = 3; d(pow(4,x))/dx != " + std::to_string(64*std::log(4)));
+    x.setValue(0);
+    assertOrExit(std::abs((pow(x, 5)).differentiate(x).evaluate() - 0) < 1.e-4, "x = 0; d(pow(x,5))/dx != 0");
     return 0;
 }
 
