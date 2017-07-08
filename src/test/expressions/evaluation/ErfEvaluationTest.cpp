6cd76b491d9bfaa264e7b9a83351999958b9b798
rcalxrc08 <rcalxrc08@users.noreply.github.com>
2017-07-08 21:16:13 +0200

    Update ErfEvaluationTest.cpp

--- src/test/expressions/evaluation/ErfEvaluationTest.cpp
+++ src/test/expressions/evaluation/ErfEvaluationTest.cpp
@@ -8,7 +8,7 @@ int main (int /*argc*/, char** /*argv*/)
 {
     Variable x;
     x.setValue(3);
-    assertOrExit(erf(x).evaluate() == std::erf(3), "e ^ " + std::to_string(x.evaluate()) + " != " + std::to_string(std::exp(3)));
+    assertOrExit(erf(x).evaluate() == std::erf(3), "e ^ " + std::to_string(x.evaluate()) + " != " + std::to_string(std::erf(3)));
     return 0;
 }
 
